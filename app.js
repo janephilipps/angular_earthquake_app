7fa9f09cf0a1ed2ee4999a47cc151ad582fb34d7
Jane Philipps <jane.philipps@gmail.com>
2015-08-26 18:48:15 -0700

    add top navbar

--- app.js
+++ app.js
@@ -2,4 +2,18 @@ angular.module('earthquakeApp', [])
   
   .controller("MainCtrl", ['$scope', function ($scope) {
 
-  }]);
+  }])
+
+  .controller("TopNavbarCtrl", ['$scope', function ($scope) {
+
+  }])
+
+  .directive('topNavbar', function () {
+    return {
+      templateUrl: 'topnavbar.html',
+      controller: 'TopNavbarCtrl'
+    };
+  })
+
+
+  ;
