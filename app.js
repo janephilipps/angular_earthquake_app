ffe93bbd1d94b85bb5490b6a307e148af1f5a3c0
Jane Philipps <jane.philipps@gmail.com>
2015-08-26 22:24:16 -0700

    add geolocation

--- app.js
+++ app.js
@@ -5,8 +5,30 @@ angular.module('earthquakeApp', [])
 
     $scope.earthquakes = [];
 
-    // Initialize API call
-      $http.get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2015-08-25&endtime=2015-08-26&latitude=37&longitude=-122&maxradius=10')
+    // var x = document.getElementById("demo");
+
+    $scope.getLocation = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition($scope.showPosition);
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    }
+
+    $scope.showPosition = function (position) {
+      $scope.latitude = position.coords.latitude;
+      $scope.longitude = position.coords.longitude;
+      console.log("Latitude: " + $scope.latitude + " Longitude: " + $scope.longitude);
+      $scope.searchEarthquakes();
+    }
+
+    $scope.searchEarthquakes = function () {
+
+      // Initialize city varible
+      var city = $scope.city;
+
+      // Initialize API call
+      $http.get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2015-08-25&endtime=2015-08-26&latitude=' + $scope.latitude + '&longitude=' + $scope.longitude + '&maxradius=10')
         .success(function (data) {
 
           // On success, create new var earthquakeData
@@ -22,6 +44,7 @@ angular.module('earthquakeApp', [])
           console.log("There was an error");
 
         })
+    }
 
   }])
 
