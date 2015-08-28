1703deeba8cbdd16cc7239f2a7746cb21c8a9238
Jane Philipps <jane.philipps@gmail.com>
2015-08-27 20:49:42 -0700

    fix ng-repeat

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ angular.module('earthquakeApp', [])
 
     $scope.earthquakes = [];
 
-    // var x = document.getElementById("demo");
+    $scope.searchByCity = function (city) {
+
+      // Initialize city varible
+      var city = $scope.city;
+
+      // Call OpenWeather API to get lat & lon coordinates
+      // Set url var
+      var url = "http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q=";
+
+      $http.jsonp(url + $scope.city)
+        .success(function(data) {
+          $scope.coords = data;
+        });
+    }
 
     $scope.getLocation = function () {
         if (navigator.geolocation) {
@@ -29,7 +42,7 @@ angular.module('earthquakeApp', [])
     $scope.searchEarthquakes = function () {
 
       // Initialize city varible
-      var city = $scope.city;
+      // var city = $scope.city;
 
       // Initialize API call
       $http.get('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2015-08-25&endtime=2015-08-26&latitude=' + $scope.latitude + '&longitude=' + $scope.longitude + '&maxradius=10')
@@ -37,9 +50,10 @@ angular.module('earthquakeApp', [])
 
           // On success, create new var earthquakeData
           var earthquakeData = data.features;
+          console.log(earthquakeData);
 
           // Push earthquakeData into earthquakes
-          $scope.earthquakes.push(earthquakeData);
+          $scope.earthquakes = earthquakeData;
 
         })
         .error(function (data) {
